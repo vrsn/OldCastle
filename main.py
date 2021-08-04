7b5446f6b108a44fbc12c83da5359c781393caf7
vrsn <rilrai1989@gmail.com>
2021-08-04 20:10:03 +0300

    Added coins and sound. Added levels and scaling of the maze. Added some TODO ideas.

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ SCREEN_TITLE = "Old Castle Maze"
 PLAYER_CHAR_SCALING = 0.35
 PLAYER_MOVEMENT_SPEED = 8
 
+LEFT_VIEWPORT_MARGIN = SCREEN_WIDTH / 2
+RIGHT_VIEWPORT_MARGIN = SCREEN_WIDTH / 2
+BOTTOM_VIEWPORT_MARGIN = SCREEN_HEIGHT / 2
+TOP_VIEWPORT_MARGIN = SCREEN_HEIGHT / 2
+
+
 TILE_SCALING = 0.25
+WALL_TILE_SIZE = 128 * TILE_SCALING
+MAZE_STARTING_WIDTH = 4
+MAZE_STARTING_HEIGHT = 4
 
-LEFT_VIEWPORT_MARGIN = SCREEN_WIDTH/2
-RIGHT_VIEWPORT_MARGIN = SCREEN_WIDTH/2
-BOTTOM_VIEWPORT_MARGIN = SCREEN_HEIGHT/2
-TOP_VIEWPORT_MARGIN = SCREEN_HEIGHT/2
-
-WALL_TILE_SIZE = 32
-HORIZONTAL_START = 30
-VERTICAL_START = 1000
-
+# TODO: Create a menu to control the game
+# TODO: Track the time for each level
+# TODO: Add animation to character movement
+# TODO: Think about the obstacles
 
 class OldCastleGame(arcade.Window):
     def __init__(self):
@@ -29,6 +33,7 @@ class OldCastleGame(arcade.Window):
         # Init the objects lists
         self.player_list = None
         self.wall_list = None
+        self.coin_list = None
 
         # Character
         self.player_char = None
@@ -40,28 +45,67 @@ class OldCastleGame(arcade.Window):
         self.view_bottom = 0
         self.view_left = 0
 
+        # Sound
+        self.collect_coin_sound = arcade.load_sound("sounds/coin1.wav")
+
+        # Set background color
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+        # Initialize global counters
+        self.coin_counter = None
+        self.maze_size = 0
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
+        # As we are using 5 bricks to build a wall, we are using this magic number.
+        # TODO: Fix magic numbers with the constants. Change logic in the maze_generator accordingly.
+        maze_cell_width = MAZE_STARTING_WIDTH + self.maze_size
+        maze_cell_height = MAZE_STARTING_HEIGHT + self.maze_size
+        maze_width = maze_cell_width * 5 * WALL_TILE_SIZE
+        maze_height = maze_cell_height * 5 * WALL_TILE_SIZE
+        vertical_start = WALL_TILE_SIZE + maze_height
+
+        self.coin_counter = 0
+
         # Create the Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
 
         # Create the player character
         image_source = "images/player/player_stand.png"
         self.player_char = arcade.Sprite(image_source, PLAYER_CHAR_SCALING)
-        self.player_char.center_x = 500
-        self.player_char.center_y = 300
+        self.player_char.center_x = (vertical_start - WALL_TILE_SIZE) / 2 + WALL_TILE_SIZE
+        self.player_char.center_y = (WALL_TILE_SIZE + maze_width) / 2 + WALL_TILE_SIZE
         self.player_list.append(self.player_char)
 
         # Create the walls
-        coordinate_list = wall_list_builder.create_wall_list(8, 8, VERTICAL_START, HORIZONTAL_START, WALL_TILE_SIZE)
+        print(WALL_TILE_SIZE)
+        coordinate_list = wall_list_builder.create_wall_list(
+                        maze_cell_width,
+                        maze_cell_height,
+                        vertical_start,
+                        WALL_TILE_SIZE,
+                        WALL_TILE_SIZE
+        )
 
         for coordinate in coordinate_list:
             wall = arcade.Sprite("images/obstacles/brickGrey.png", TILE_SCALING)
             wall.position = coordinate
             self.wall_list.append(wall)
+
+        # Create coins in 4 corners of the maze
+        coin_coordinates = [
+            ((WALL_TILE_SIZE * 4.5), (maze_height - WALL_TILE_SIZE * 1.5)),                 # top left
+            ((maze_width - WALL_TILE_SIZE * 0.5), (maze_height - WALL_TILE_SIZE * 1.5)),    # top right
+            ((maze_width - WALL_TILE_SIZE * 0.5), (WALL_TILE_SIZE * 3.5)),                  # bottom right
+            ((WALL_TILE_SIZE * 4.5), (WALL_TILE_SIZE * 3.5))                                # bottom left
+        ]
+
+        for coordinate in coin_coordinates:
+            new_coin = arcade.Sprite("images/items/coinGold.png", TILE_SCALING)
+            new_coin.position = coordinate
+            self.coin_list.append(new_coin)
 
         # Physics engine
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_char, self.wall_list)
@@ -74,6 +118,7 @@ class OldCastleGame(arcade.Window):
 
         self.wall_list.draw()
         self.player_list.draw()
+        self.coin_list.draw()
 
     def on_key_press(self, key, modifiers):
         """ Called whenever a key is pressed. """
@@ -85,6 +130,11 @@ class OldCastleGame(arcade.Window):
             self.player_char.change_x = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_char.change_x = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.ENTER:
+            if self.coin_counter == 4:
+                self.maze_size += 1
+                self.setup()
+
 
     def on_key_release(self, key, modifiers):
         """ Called when the user releases a key. """
@@ -102,10 +152,25 @@ class OldCastleGame(arcade.Window):
         # Move the player with the physics engine
         self.physics_engine.update()
 
+        # See if we hit any coins
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_char, self.coin_list)
+
+        # Loop through each coin we hit (if any) and remove it
+        for coin in coin_hit_list:
+            # Remove the coin
+            coin.remove_from_sprite_lists()
+            # Play a sound
+            arcade.play_sound(self.collect_coin_sound)
+            # Update coin counter
+            self.coin_counter += 1
+
+            if self.coin_counter > 3:
+                # TODO change to a menu.
+                print("VICTORY")
+
         # --- Manage Scrolling ---
 
         # Track if we need to change the viewport
-
         changed = False
 
         # Scroll left
--- maze_generator.py
+++ maze_generator.py
@@ -2,33 +2,30 @@ from random import shuffle
 
 
 def make_maze(width, height):
-    vis = [[0] * width + [1] for _ in range(height)] + [[1] * (width + 1)]
-    ver = [[0] * width + [0] for _ in range(height)] + [[]]
-    hor = [[0] * width for _ in range(height + 1)]
+    # Keep track of visited cells
+    visited = [[0] * width + [1] for _ in range(height)] + [[1] * (width + 1)]
+    # Where in the vertical walls we need to have open path. 0 is solid wall, 1 is path.
+    vertical_paths = [[0] * width + [0] for _ in range(height)] + [[]]
+    # Where in the horizontal walls we need to have open path. 0 is solid wall, 1 is path.
+    horizontal_paths = [[0] * width for _ in range(height + 1)]
 
     def walk(x, y):
-        vis[y][x] = 1
+        visited[y][x] = 1
 
-        d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
-        shuffle(d)
+        neighbour_cells = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+        shuffle(neighbour_cells)
 
-        for (xx, yy) in d:
-            if vis[yy][xx]:
+        for (xx, yy) in neighbour_cells:
+            if visited[yy][xx]:
                 continue
             if xx == x:
-                hor[max(y, yy)][x] = 1
+                horizontal_paths[max(y, yy)][x] = 1
             if yy == y:
-                ver[y][max(x, xx)] = 1
+                vertical_paths[y][max(x, xx)] = 1
 
+            # Recursive call
             walk(xx, yy)
 
     walk(0, 0)
 
-    return hor, ver
-
-
-if __name__ == '__main__':
-    vertical, horizontal = make_maze(5,5)
-    for vert, hori in zip(vertical, horizontal):
-        print(hori)
-        print(vert)
+    return horizontal_paths, vertical_paths
